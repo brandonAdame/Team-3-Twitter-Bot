6559fe3de88b5425751139410fbdbeb0eb8c61f6
brandonAdame <brandonadame29@yahoo.com>
2018-09-25 20:26:42 -0400

    first run successful

--- twit.py
+++ twit.py
@@ -1,11 +1,6 @@
 import tweepy
-"""
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
-"""
-
+# credentials
 consumer_key = "ThbfGVBrpRwMKu9FVgR6HjA1m"
 consumer_secret = "lGyzD69pQGupB4lTG3jWG8rszYmVN4CGjFPYGUBTr1EhKdiBxh"
 access_token = 	"1039183691510165505-IkoKTm8MopQ3PzYVmEgU2NdGmognPL"
@@ -15,7 +10,10 @@ access_token_secret = "jwWe8WqunRcmqKWgvYyDuUjkyotgfUddeLKcTHYz40ktP"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-# connect through a secure connection
+# autheticating
 api = tweepy.API(auth)
 
-api.update_profile('I bet you came here for the weather')
+print('authentication successful')
+
+phrase = 'get a rain jacket'
+api.update_status(phrase)
