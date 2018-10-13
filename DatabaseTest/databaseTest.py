206d1b58f36e76875b95babfbc526c60d5dee4b6
DanielLeeMeeks <daniellmeeks@gmail.com>
2018-10-13 14:09:10 -0400

    Added login info for database

--- DatabaseTest/databaseTest.py
+++ DatabaseTest/databaseTest.py
@@ -1,1 +1,11 @@
+import MySQLdb
 
+conn = MySQLdb.connect(host=35.196.238.195, user=root, passwd=85v6EGGnz8FOmcgD, db=TwitterBot)
+cursor = conn.cursor()
+
+cursor.execute('SELECT * FROM events')
+row = cursor.fetchone()
+
+conn.close()
+
+print(row)
