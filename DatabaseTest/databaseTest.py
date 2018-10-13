94b9ad8eaf5548f085e89f3a03934965f547c85d
DanielLeeMeeks <daniellmeeks@gmail.com>
2018-10-13 14:11:22 -0400

    Update databaseTest.py

--- DatabaseTest/databaseTest.py
+++ DatabaseTest/databaseTest.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
-conn = MySQLdb.connect(host=35.196.238.195, user=root, passwd=85v6EGGnz8FOmcgD, db=TwitterBot)
+conn = MySQLdb.connect(host="35.196.238.195", user="root", passwd="85v6EGGnz8FOmcgD", db="TwitterBot")
 cursor = conn.cursor()
 
 cursor.execute('SELECT * FROM events')
