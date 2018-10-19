cae39c3f32a8e9a177539708171d6ae2d418f405
Nicholas Ellis <31054604+FlyingHermit@users.noreply.github.com>
2018-10-19 13:26:36 -0400

    Update main.py

--- API Test/main.py
+++ API Test/main.py
@@ -1,40 +1,61 @@
-import requests #Python HTTP for Humans.
+#=========================================================================================
+#										 Imports
+#=========================================================================================
+import requests 	#Python HTTP for Humans.
 import feedparser
 import time
 import tweepy
+import zipcodes		#zipcodes database
 
+#=========================================================================================
+#										 API Keys
+#=========================================================================================
+#OpenWeatherMap Key
 apiKey = "07a6ed1ad10a5c97fa9daa3c5babcaab"
 
-#Twitter keys
-#@CSCIteam3
-consumer_key = "ThbfGVBrpRwMKu9FVgR6HjA1m"
-consumer_secret = "lGyzD69pQGupB4lTG3jWG8rszYmVN4CGjFPYGUBTr1EhKdiBxh"
-access_token = "1039183691510165505-IkoKTm8MopQ3PzYVmEgU2NdGmognPL"
-access_token_secret = "jwWe8WqunRcmqKWgvYyDuUjkyotgfUddeLKcTHYz40ktP"
+#Twitter keys (@CSCIteam3)
+twitter_consumer_key = "ThbfGVBrpRwMKu9FVgR6HjA1m"
+twitter_consumer_key_secret = "lGyzD69pQGupB4lTG3jWG8rszYmVN4CGjFPYGUBTr1EhKdiBxh"
+twitter_access_key = "1039183691510165505-IkoKTm8MopQ3PzYVmEgU2NdGmognPL"
+twitter_access_key_secret = "jwWe8WqunRcmqKWgvYyDuUjkyotgfUddeLKcTHYz40ktP"
 
 #Tweepy authentication
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_key_secret)
+auth.set_access_token(twitter_access_key, twitter_access_key_secret)
 api = tweepy.API(auth)
 
 
-# Raleigh, NC 27834
+
+#=========================================================================================
+#									Implementation
+#=========================================================================================
+# 					Get weather for Greenville, NC 27858 and tweet it.
+#-----------------------------------------------------------------------------------------
+
+# Greenville, NC 27858
 zipCode = "27858"
 apiURL = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&appid=" + apiKey + "&units=imperial"
+
+
+#Uses zipcode to get city name and state.
+zipCodeInfo = zipcodes.matching(zipCode)
+zipCodeInfo = zipCodeInfo[0]
 
 #Get web request
 response = requests.get(apiURL).json()
 
 #Store in easy to use variables
 currentTemp = response["main"]["temp"]
-location = response["name"]
-coord = str( response["coord"]["lon"] )+ ", " + str(response["coord"]["lat"])
-high = response["main"]["temp_max"]
-low = response["main"]["temp_min"]
-winds = response["wind"]["speed"]
+location 	= zipCodeInfo["city"] +", "+ zipCodeInfo["state"]	
+coord 		= str( response["coord"]["lon"] )+ ", " + str(response["coord"]["lat"])
+high 		= response["main"]["temp_max"]
+low 		= response["main"]["temp_min"]
+winds 		= response["wind"]["speed"]
 description = response["weather"][0]["description"]
 
 #Print the forcast
-#message = "The weather in " + location + " (" + coord + ") is " + description + ".\nThe temperature is currently " + str(currentTemp) + " *F with a high of " + str(high) + " *F and a low of " + str(low) + " *F.\nThe wind speed is " + str(winds) + " MPH.")
+print("The weather in " + location + " (" + coord + ") is " + description + ".\nThe temperature is currently " + str(currentTemp) + " *F with a high of " + str(high) + " *F and a low of " + str(low) + " *F.\nThe wind speed is " + str(winds) + " MPH.")
 
 api.update_status(status="The weather in " + location + " (" + coord + ") is " + description + ".\nThe temperature is currently " + str(currentTemp) + " *F with a high of " + str(high) + " *F and a low of " + str(low) + " *F.\nThe wind speed is " + str(winds) + " MPH.")
+
+
