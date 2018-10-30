2a3ac113bb2df91fbbd0abb4879a7e449db02cce
DanielLeeMeeks <daniellmeeks@gmail.com>
2018-10-30 20:14:58 +0000

    Change to tweet time every min

--- API Test/timeTest.py
+++ API Test/timeTest.py
@@ -1,3 +1,27 @@
 import datetime
+import time
+import tweepy
 
-print(datetime.datetime.now())
+while (true)
+  print(datetime.datetime.now())
+  tweet("The time is " + str(datetime.datetime.now()) );
+  time.sleep(60)
+
+def tweet(message):
+  try:
+    #@DanielLeeMeeks2
+    consumer_key = "U01EcJ8sqBgSaafvPW1u3nHzR"
+    consumer_secret = "tt9UdUqKXHVDpJguTyS1fKhTmPxUHq2VRKaqg056ZirIGH3tgA"
+    access_key = "3904219755-szrud8F89KgaAPLzjXpKbIWIvVvIqrkeOXLd1vd"
+    access_secret = "DBwqE09xXK69tHHlqqv9pFwY0u2V3APoSOjRLVuPJO8Kn"
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    ##if (len(message) > 280):
+    ##  message = message[0:275] + "...";
+    ##  if (len(message) + len(link) + 5 > 280 ):
+    ##    message = message[0:280-len(link)-5] + "..."
+    api.update_status(status=message)
+  except:
+    print("There was an error while tweeting.")
