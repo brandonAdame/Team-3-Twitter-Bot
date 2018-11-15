f9046092ef638f9ae532efa2f017b58908c8bd0f
DanielLeeMeeks <daniellmeeks@gmail.com>
2018-11-15 20:29:19 +0000

    Update checkDMs.py

--- TwitterAPI_Test/checkDMs.py
+++ TwitterAPI_Test/checkDMs.py
@@ -5,6 +5,7 @@ consumer_key = "ThbfGVBrpRwMKu9FVgR6HjA1m"
 consumer_secret = "lGyzD69pQGupB4lTG3jWG8rszYmVN4CGjFPYGUBTr1EhKdiBxh"
 access_key = "1039183691510165505-IkoKTm8MopQ3PzYVmEgU2NdGmognPL"
 access_secret = "jwWe8WqunRcmqKWgvYyDuUjkyotgfUddeLKcTHYz40ktP"
+twitterID = "1039183691510165505"
 
 api = TwitterAPI(consumer_key, consumer_secret, access_key, access_secret)
 
@@ -12,4 +13,7 @@ api = TwitterAPI(consumer_key, consumer_secret, access_key, access_secret)
 #Get DMs from last 30 days
 r = api.request('direct_messages/events/list')
 print(r.status_code)
-print(r.json())
+#print(r.json())
+
+for message in r.json():
+  print (r.json()["events"])
