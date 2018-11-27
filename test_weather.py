54e8303b57b095b9959dc028bf1929dfb172f108
Brandon Gachuz <gachuzb17@students.ecu.edu>
2018-11-27 12:42:55 -0500

    Added testing module for weather.py

--- test_weather.py
+++ test_weather.py
@@ -1,1 +1,19 @@
 import unittest
+import requests 	
+import zipcodes		
+import datetime		
+from bs4 import BeautifulSoup
+
+'''
+This module was created to test weather.py
+'''
+
+class Testweather(unittest.TestCase):
+    def test_get_high_lows(self):
+        pass
+
+    def test_get_weather(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main
