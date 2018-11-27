d120edc5d2c922b300c8546221406d8cb7d37d6c
Brandon Gachuz <gachuzb17@students.ecu.edu>
2018-11-26 21:07:57 -0500

    Did some refactoring

--- test_getDailyQuote.py
+++ test_getDailyQuote.py
@@ -1,9 +1,11 @@
 import unittest
+from getDailyQuote import get_daily_quote
 from requests import get
 from bs4 import BeautifulSoup
 
+# How to run tests: python -m unittest test_brainyquote.py
+
 class TestgetDailyQuote(unittest.TestCase):
 	def test_get_daily_quote(self):
-	
-	curr_quote = "One that would have the fruit must climb the tree\n\t-Thomas Fuller"
-	self.assertEquals(curr_quote, get_daily_quote)
+		curr_quote = "One that would have the fruit must climb the tree\n-Thomas Fuller"
+		self.assertEquals(curr_quote, get_daily_quote)
