84e88dc3a8b0f3b6339b4137460a5bb4be2d0931
Trevor Downey <downeyt16@students.ecu.edu>
2018-11-27 12:48:16 -0500

    Added guidelines for unit testing

--- test_getDailyQuote.py
+++ test_getDailyQuote.py
@@ -5,17 +5,20 @@ from bs4 import BeautifulSoup
 
 # How to run tests: python -m unittest test_getDailyQuote.py
 
+#NOTE: The quote of the day needs to be manually pulled from "https://www.brainyquote.com/quote_of_the_day"
+actualQuote = get_daily_quote()
 class TestgetDailyQuote(unittest.TestCase):
+
     # Test 1 (s = manual typed daily quote)
     def test_get_daily_quote(self):
-        s = get_daily_quote()
-        self.assertEqual(s, "Who covets more is evermore a slave.\n-Robert Herrick")
+        self.assertEqual(actualQuote, "The great advantage about telling the truth is that nobody ever believes it.\n-Dorothy L. Sayers")
     #Test 1: passed
         
-    # Test 2 (t != manual typed daily quote)
+    # Test 2 (actualQuote != manual typed daily quote)
+    # This test is designed to compare the actual daily quote with an "unequal" quote
     def test_get_daily_quote_fail(self):
-    	t = get_daily_quote()
-    	self.assertNotEqual(t, "ho covets more is evermore a slave.\n-Robert Herrick")    
+    	self.assertNotEqual(actualQuote, "ho covets more is evermore a slave.\n-Robert Herrick")    
     # Test 2: passed	
+    
 if __name__ == '__main__':
     unittest.main
