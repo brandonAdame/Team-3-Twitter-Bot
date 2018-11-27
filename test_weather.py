9b4ab979577cea833c74535184c3e09f4c3c0e39
Brandon Gachuz <gachuzb17@students.ecu.edu>
2018-11-27 13:31:42 -0500

    Uploading tests

--- test_weather.py
+++ test_weather.py
@@ -3,9 +3,14 @@ import requests
 import zipcodes		
 import datetime		
 from bs4 import BeautifulSoup
+from weather import getHighLows, getWeather
 
 '''
-This module was created to test weather.py
+How to run: 
+    1. Make sure virtual environment in active: source Team-3-Twitter-Bot/bin/activate
+    2. Run the following command in bash: python -m unittest test_weather.py
+
+This module tests the reqirement for: weather
 '''
 
 class Testweather(unittest.TestCase):
@@ -13,7 +18,14 @@ class Testweather(unittest.TestCase):
         pass
 
     def test_get_weather(self):
-        pass
+        """
+        The 'weather' variable is manually written each day to match with getWeather() return value
+        """
+        curr_forecast = getWeather('27889')
+        weather = "The weather in Washington, NC is clear sky. The temperature is currently 47.14 *F. Max: 49 / Min: 28"
+        self.assertEqual(weather, getWeather('27889'))
+        self.assertNotEqual("This is a weather forecast", curr_forecast)
+
 
 if __name__ == '__main__':
     unittest.main
