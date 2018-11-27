d0a07b975682657097d59e0a1e736ac3aad49ad1
Brandon Gachuz <gachuzb17@students.ecu.edu>
2018-11-26 21:52:10 -0500

    Reformatted

--- test_getDailyQuote.py
+++ test_getDailyQuote.py
@@ -3,9 +3,13 @@ from getDailyQuote import get_daily_quote
 from requests import get
 from bs4 import BeautifulSoup
 
-# How to run tests: python -m unittest test_brainyquote.py
+# How to run tests: python -m unittest test_getDailyQuote.py
 
 class TestgetDailyQuote(unittest.TestCase):
 	def test_get_daily_quote(self):
-		curr_quote = "One that would have the fruit must climb the tree\n-Thomas Fuller"
-		self.assertEquals(curr_quote, get_daily_quote)
+		# curr_quote = "Who covets more is evermore a slave.\n-Robert Herrick"
+		s = get_daily_quote()
+		self.assertEqual(s, "Who covets more is evermore a slave.\n-Robert Herrick")
+
+if __name__ == '__main__':
+    unittest.main
