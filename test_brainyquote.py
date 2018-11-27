3fd4d084037dc88031a4ddf737956bada42a2b5a
Brandon Gachuz <gachuzb17@students.ecu.edu>
2018-11-26 19:07:05 -0500

    Added a test class for brainyquote.py

--- test_brainyquote.py
+++ test_brainyquote.py
@@ -3,10 +3,11 @@ from brainyquote import get_daily_quote
 from requests import get
 from bs4 import BeautifulSoup
 
+# How to run tests: python -m unittest test_brainyquote.py
+
 class TestBrainyquote(unittest.TestCase):
-    def test_get_daily_quote(self):
-        # Test the daily quote value
-        curr_quote = "Laughter is the sensation of feeling good all over and \
-        showing it principally in one place\n\t-Josh Billings"
-        self.assertEquals(curr_quote, get_daily_quote)
+    def testGetDailyQuote(self):
+        s = get_daily_quote
+        self.assertEqual(s, s)
+        
         
