aedcd5f996903e4e96941a77669a5cc24c5d43d8
Brandon Gachuz <gachuzb17@students.ecu.edu>
2018-11-27 12:34:17 -0500

    Made some changes

--- test_getDailyQuote.py
+++ test_getDailyQuote.py
@@ -6,10 +6,13 @@ from bs4 import BeautifulSoup
 # How to run tests: python -m unittest test_getDailyQuote.py
 
 class TestgetDailyQuote(unittest.TestCase):
-    def test_get_daily_quote(self):
-        # curr_quote = "Who covets more is evermore a slave.\n-Robert Herrick"
-        s = get_daily_quote()
-        self.assertEqual(s, "Who covets more is evermore a slave.\n-Robert Herrick")
+
+	def test_get_daily_quote(self):
+		curr_quote = "Who covets more is evermore a slave.\n-Robert Herrick"
+		s = get_daily_quote()
+		self.assertEqual(s, curr_quote)
+		self.assertNotEqual(s, "lkjlk")
+    
 
 if __name__ == '__main__':
     unittest.main
